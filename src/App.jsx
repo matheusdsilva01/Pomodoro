f3bdabc81178e860d000d80ed655dc6538f5f5ba
Matheus da silva <ms25022003@gmail.com>
2022-01-07 14:31:45 -0300

    estrutura projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(25);
+
+  useEffect(() => {
+    const myInterval = setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+    if (time === 0) {
+      clearTimeout(myInterval);
+    }
+  }, [time]);
+
   return (
     <>
+      <h1>hello world</h1>
+      <h1>{time}</h1>
     </>
   );
 }
